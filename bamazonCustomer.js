8f586cd82292360bb779d632af1d30908374fd68
Handrew <andrew.a.han@gmail.com>
2018-04-28 18:16:11 -0400

    completed challenge one and fixed all the bugs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ const conn = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "vtr4k8hp",
+    password: "",
     database: "bamazon"
 });
 
@@ -17,23 +17,43 @@ conn.connect(function (err) {
 
 // Inquirer - to ask two questions inside a function to call in the connection
 function askingTwoQuestions() {
+
+    conn.query("SELECT * FROM products", function(req, resp) {  //start of part 2 of displaying all the items
+
     inquirer.prompt({
         name: "firstQuestion",
-        type: "input",
-        message: "What is the ID of the product you would like to buy? (1 - 10)"
+        type: "list",
+        message: "What is the ID of the product you would like to buy?",
+        choices: [
+            `ID#: ${resp[0].item_id} Product: ${resp[0].product_name} Price $${resp[0].price}` ,
+            `ID#: ${resp[1].item_id} Product: ${resp[1].product_name} Price $${resp[1].price}`,
+            `ID#: ${resp[2].item_id} Product: ${resp[2].product_name} Price $${resp[2].price}`,
+            `ID#: ${resp[3].item_id} Product: ${resp[3].product_name} Price $${resp[3].price}`,
+            `ID#: ${resp[4].item_id} Product: ${resp[4].product_name} Price $${resp[4].price}`,
+            `ID#: ${resp[5].item_id} Product: ${resp[5].product_name} Price $${resp[5].price}`,
+            `ID#: ${resp[6].item_id} Product: ${resp[6].product_name} Price $${resp[6].price}`,
+            `ID#: ${resp[7].item_id} Product: ${resp[7].product_name} Price $${resp[7].price}`,
+            `ID#: ${resp[8].item_id} Product: ${resp[8].product_name} Price $${resp[8].price}`,
+            `ID#: ${resp[9].item_id} Product: ${resp[9].product_name} Price $${resp[9].price}`
+        ]
     }).then(function (answer) {
         //===== query start
 
-        conn.query('SELECT * FROM products WHERE ?', { item_id: answer.firstQuestion }, function (err, res) {
-
+        //using regex positive lookahead and look behind to grab whats between ID and Product from the choices
+        var regex = /(?<=ID#: )(.*)(?= Product:)/g;
+        var selected = answer.firstQuestion.match(regex);
+        console.log(answer.firstQuestion);
+        conn.query('SELECT * FROM products WHERE ?', { item_id: selected }, function (err, res) {
+            console.log(res);
             function incaseOfInsufficient() {
                 inquirer.prompt({
                     name: "secondQuestion",
                     type: "input",
-                    message: "You have selected to buy ID #" + answer.firstQuestion + ", which is \n\n" + res[0].product_name + ". \n\nOne of this product is $" + res[0].price + "\n\nHow many would you like to buy?  "
+                    message: "You have selected to buy ID #" + res[0].item_id + ", which is \n\n" + res[0].product_name + ". \n\nOne of this product is $" + res[0].price + "\n\nHow many would you like to buy?  "
                 }).then(function (answer2) {
                     //parsing the answer to a number
                     var numbered = parseInt(answer2.secondQuestion);
+                    console.log(numbered);
 
                     if ((numbered > parseInt(res[0].stock_quantity))) {
                         console.log("Insufficient quantity!");
@@ -41,25 +61,28 @@ function askingTwoQuestions() {
                         setTimeout(incaseOfInsufficient, 2000);
 
                     } else {
+                        
+
                         var newStock = (parseInt(res[0].stock_quantity) - numbered);
                     
                         console.log("You have asked to buy " + numbered + ". \nThe total price is $" + (parseInt(res[0].price) * numbered) + ".00\n\n\n");
 
                         var setWhat = "stock_quantity = " + newStock;
-                        var atWhatItemId = "item_id = " + answer.firstQuestion;
+                        var atWhatItemId = "item_id = " + selected;
 
                         conn.query("UPDATE products SET "+ setWhat +" WHERE "+atWhatItemId, function (err, res1) {
                             if(err) console.log("failed");
                             
-                            console.log(res1);
+                            console.log("We have placed your order!");
                         });
 
-                        conn.query("SELECT * FROM products WHERE item_id = " + answer.firstQuestion, function(err, res2) {
+                        conn.query("SELECT * FROM products WHERE item_id = " + selected, function(err, res2) {
                             if(err) console.log("error 2");
                             var updatedStock = res2[0].stock_quantity;
-                            console.log(`We have ${updatedStock} of ${res[0].product_name} left.`);
+                            console.log(`We have ${updatedStock} of ${res[0].product_name} left. \n`);
                         })
                         setTimeout(buyAgain, 2000);
+
                     }
 
                 });
@@ -70,6 +93,9 @@ function askingTwoQuestions() {
         });
 
     });
+
+});// end of part 2 of getting the display of all items
+
 };
 
 function buyAgain() {
--- bamazonManager.js
+++ bamazonManager.js
@@ -6,7 +6,7 @@ const conn = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "vtr4k8hp",
     database: "bamazon"
 });
 
@@ -17,9 +17,9 @@ conn.connect(function (err) {
 
 function repeatToFront() {
 
-//====================Inquirer START ===============================
+    //====================Inquirer START ===============================
     inquirer.prompt({
-        name:"listOfSetMenu",
+        name: "listOfSetMenu",
         type: "list",
         choices: [
             "View Products for Sale",
@@ -28,51 +28,67 @@ function repeatToFront() {
             "Add New Product"
         ],
         message: "Menu List Options"
-    }).then(function(response) {
+
+    }).then(function (response) {
         var list = response.listOfSetMenu;
 
+        //Switch Statement
+        switch (list) {
+            case "View Products for Sale":
+                conn.query("SELECT * FROM products", function (req, res) {
+                    var lineSpace = '\n =============== \n';
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(`Item ID: ${res[i].item_id} \nNames: ${res[i].product_name} \nPrices: ${res[i].price} \nQuantities: ${res[i].stock_quantity} ${lineSpace}`);
+                    };
+                })
+
+                break;
+
+            case "View Low Inventory":
+
+                conn.query("SELECT * FROM products WHERE stock_quantity < 5", function (req, res) {
+                    var lineSpace = '\n =============== \n';
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(`Item ID: ${res[i].item_id} \nNames: ${res[i].product_name} \nPrices: ${res[i].price} \nQuantities: ${res[i].stock_quantity} ${lineSpace}`);
+                    };
+                })
+
+                break;
+
+            case "Add to Inventory":
+                conn.query("SELECT * FROM products", function(req, res1) {                  
+                
+                inquirer.prompt({
+                    name: "adding",
+                    type: "list",
+                    message: "Which of the items would you like to add more to stock?",
+                    choices: [
+                        res1[0].product_name,
+                        res1[1].product_name,
+                        res1[2].product_name,
+                        res1[3].product_name,
+                        res1[4].product_name,
+                        res1[5].product_name,
+                        res1[6].product_name,
+                        res1[7].product_name,
+                        res1[8].product_name,
+                        res1[9].product_name
+                    ]
+                })
+            });
+                break;
+
+            case "Add New Product":
+                for (var i = 0; i < res.length; i++) {
+                    console.log(res[i].item_id);
+                };
+                break;
 
 
-    //Switch
-    switch(list) {
-        case "View Products for Sale":
-        conn.query("SELECT * FROM products", function(req, res){
-            var lineSpace = '\n =============== \n';
-            for (var i =0; i<res.length; i++) {
-                console.log(`Item ID: ${res[i].item_id} \nNames: ${res[i].product_name} \nPrices: ${res[i].price} \nQuantities: ${res[i].stock_quantity} ${lineSpace}`);
-            };
-        })
-
-        break;
-
-        case "View Low Inventory":
-
-        conn.query("SELECT * FROM products WHERE stock_quantity < 5", function (req, res) {
-            var lineSpace = '\n =============== \n';
-            for (var i =0; i<res.length; i++) {
-                console.log(`Item ID: ${res[i].item_id} \nNames: ${res[i].product_name} \nPrices: ${res[i].price} \nQuantities: ${res[i].stock_quantity} ${lineSpace}`);
-            };
-        })
-
-        break;
-
-        case "Add to Inventory":
-        for (var i =0; i<res.length; i++) {
-            console.log(res[i].item_id);
-        };
-        break;
-
-        case "Add New Product":
-        for (var i =0; i<res.length; i++) {
-            console.log(res[i].item_id);
-        };
-        break;
+        }
 
 
-    }
-
-        
     })
-//====================Inquirer END ===============================
+    //====================Inquirer END ===============================
 
 };
