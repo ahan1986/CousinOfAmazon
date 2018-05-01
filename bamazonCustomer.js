2efde43f95554842933ceb542f6cf36960d9472b
Handrew <andrew.a.han@gmail.com>
2018-04-30 20:11:01 -0400

    formatted each challange

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,81 +18,81 @@ conn.connect(function (err) {
 // Inquirer - to ask two questions inside a function to call in the connection
 function askingTwoQuestions() {
 
-    conn.query("SELECT * FROM products", function(req, resp) {  //start of part 2 of displaying all the items
+    conn.query("SELECT * FROM products", function (req, resp) {  //start of part 2 of displaying all the items
 
-    inquirer.prompt({
-        name: "firstQuestion",
-        type: "list",
-        message: "What is the ID of the product you would like to buy?",
-        choices: [
-            `ID#: ${resp[0].item_id} Product: ${resp[0].product_name} Price $${resp[0].price}` ,
-            `ID#: ${resp[1].item_id} Product: ${resp[1].product_name} Price $${resp[1].price}`,
-            `ID#: ${resp[2].item_id} Product: ${resp[2].product_name} Price $${resp[2].price}`,
-            `ID#: ${resp[3].item_id} Product: ${resp[3].product_name} Price $${resp[3].price}`,
-            `ID#: ${resp[4].item_id} Product: ${resp[4].product_name} Price $${resp[4].price}`,
-            `ID#: ${resp[5].item_id} Product: ${resp[5].product_name} Price $${resp[5].price}`,
-            `ID#: ${resp[6].item_id} Product: ${resp[6].product_name} Price $${resp[6].price}`,
-            `ID#: ${resp[7].item_id} Product: ${resp[7].product_name} Price $${resp[7].price}`,
-            `ID#: ${resp[8].item_id} Product: ${resp[8].product_name} Price $${resp[8].price}`,
-            `ID#: ${resp[9].item_id} Product: ${resp[9].product_name} Price $${resp[9].price}`
-        ]
-    }).then(function (answer) {
-        //===== query start
+        inquirer.prompt({
+            name: "firstQuestion",
+            type: "list",
+            message: "What is the ID of the product you would like to buy?",
+            choices: [
+                `ID#: ${resp[0].item_id} Product: ${resp[0].product_name} Price $${resp[0].price}`,
+                `ID#: ${resp[1].item_id} Product: ${resp[1].product_name} Price $${resp[1].price}`,
+                `ID#: ${resp[2].item_id} Product: ${resp[2].product_name} Price $${resp[2].price}`,
+                `ID#: ${resp[3].item_id} Product: ${resp[3].product_name} Price $${resp[3].price}`,
+                `ID#: ${resp[4].item_id} Product: ${resp[4].product_name} Price $${resp[4].price}`,
+                `ID#: ${resp[5].item_id} Product: ${resp[5].product_name} Price $${resp[5].price}`,
+                `ID#: ${resp[6].item_id} Product: ${resp[6].product_name} Price $${resp[6].price}`,
+                `ID#: ${resp[7].item_id} Product: ${resp[7].product_name} Price $${resp[7].price}`,
+                `ID#: ${resp[8].item_id} Product: ${resp[8].product_name} Price $${resp[8].price}`,
+                `ID#: ${resp[9].item_id} Product: ${resp[9].product_name} Price $${resp[9].price}`
+            ]
+        }).then(function (answer) {
+            //===== query start
 
-        //using regex positive lookahead and look behind to grab whats between ID and Product from the choices
-        var regex = /(?<=ID#: )(.*)(?= Product:)/g;
-        var selected = answer.firstQuestion.match(regex);
-        
-        conn.query('SELECT * FROM products WHERE ?', { item_id: selected }, function (err, res) {
-            console.log(res);
-            function incaseOfInsufficient() {
-                inquirer.prompt({
-                    name: "secondQuestion",
-                    type: "input",
-                    message: "You have selected to buy ID #" + res[0].item_id + ", which is \n\n" + res[0].product_name + ". \n\nOne of this product is $" + res[0].price + "\n\nHow many would you like to buy?  "
-                }).then(function (answer2) {
-                    //parsing the answer to a number
-                    var numbered = parseInt(answer2.secondQuestion);
+            //using regex positive lookahead and look behind to grab whats between ID and Product from the choices
+            var regex = /(?<=ID#: )(.*)(?= Product:)/g;
+            var selected = answer.firstQuestion.match(regex);
 
-                    if ((numbered > parseInt(res[0].stock_quantity))) {
-                        console.log("Insufficient quantity!");
+            conn.query('SELECT * FROM products WHERE ?', { item_id: selected }, function (err, res) {
+                console.log(res);
+                function incaseOfInsufficient() {
+                    inquirer.prompt({
+                        name: "secondQuestion",
+                        type: "input",
+                        message: "You have selected to buy ID #" + res[0].item_id + ", which is \n\n" + res[0].product_name + ". \n\nOne of this product is $" + res[0].price + "\n\nHow many would you like to buy?  "
+                    }).then(function (answer2) {
+                        //parsing the answer to a number
+                        var numbered = parseInt(answer2.secondQuestion);
 
-                        setTimeout(incaseOfInsufficient, 2000);
+                        if ((numbered > parseInt(res[0].stock_quantity))) {
+                            console.log("Insufficient quantity!");
 
-                    } else {
-                        
-                        var newStock = (parseInt(res[0].stock_quantity) - numbered);
-                    
-                        console.log("You have asked to buy " + numbered + ". \nThe total price is $" + (parseInt(res[0].price) * numbered) + ".00\n\n\n");
+                            setTimeout(incaseOfInsufficient, 2000);
 
-                        var setWhat = "stock_quantity = " + newStock;
-                        var atWhatItemId = "item_id = " + selected;
+                        } else {
 
-                        conn.query("UPDATE products SET "+ setWhat +" WHERE "+atWhatItemId, function (err, res1) {
-                            if(err) console.log("failed");
-                            
-                            console.log("We have placed your order!");
-                        });
+                            var newStock = (parseInt(res[0].stock_quantity) - numbered);
 
-                        conn.query("SELECT * FROM products WHERE item_id = " + selected, function(err, res2) {
-                            if(err) console.log("error 2");
-                            var updatedStock = res2[0].stock_quantity;
-                            console.log(`We have ${updatedStock} of ${res[0].product_name} left. \n`);
-                        })
-                        setTimeout(buyAgain, 2000);
+                            console.log("You have asked to buy " + numbered + ". \nThe total price is $" + (parseInt(res[0].price) * numbered) + ".00\n\n\n");
 
-                    }
+                            var setWhat = "stock_quantity = " + newStock;
+                            var atWhatItemId = "item_id = " + selected;
 
-                });
-            };
-            incaseOfInsufficient();
-            //=====query   end   
+                            conn.query("UPDATE products SET " + setWhat + " WHERE " + atWhatItemId, function (err, res1) {
+                                if (err) console.log("failed");
+
+                                console.log("We have placed your order!");
+                            });
+
+                            conn.query("SELECT * FROM products WHERE item_id = " + selected, function (err, res2) {
+                                if (err) console.log("error 2");
+                                var updatedStock = res2[0].stock_quantity;
+                                console.log(`We have ${updatedStock} of ${res[0].product_name} left. \n`);
+                            })
+                            setTimeout(buyAgain, 2000);
+
+                        }
+
+                    });
+                };
+                incaseOfInsufficient();
+                //=====query   end   
+
+            });
 
         });
 
-    });
-
-});// end of part 2 of getting the display of all items
+    });// end of part 2 of getting the display of all items
 
 };
 //function to ask if the user wants to buy again and if not, it will exit the app
@@ -106,7 +106,7 @@ function buyAgain() {
             "No"
         ]
     }).then(function (res) {
-        
+
         if (res.again === "Yes") {
             askingTwoQuestions()
         } else {
--- bamazonManager.js
+++ bamazonManager.js
@@ -15,6 +15,7 @@ conn.connect(function (err) {
     repeatToFront();
 });
 
+//function to call back using recursion for each questions asked in the first inquirer
 function repeatToFront() {
 
     //====================Inquirer START ===============================
@@ -49,8 +50,8 @@ function repeatToFront() {
 
                 conn.query("SELECT * FROM products WHERE stock_quantity < 5", function (req, res) {
                     var lineSpace = '\n =============== \n';
-                    
-                    if(res[0] === undefined) {
+
+                    if (res[0] === undefined) {
                         console.log("\nInventory is fully stocked. Nothing is below 5!");
                     }
                     for (var i = 0; i < res.length; i++) {
@@ -62,92 +63,91 @@ function repeatToFront() {
                 break;
 
             case "Add to Inventory":
-                conn.query("SELECT * FROM products", function(req, res1) {                  
-                
-                inquirer.prompt({
-                    name: "adding",
-                    type: "list",
-                    message: "Which of the items would you like to add more to stock?",
-                    choices: [
-                        "Id#: "+res1[0].item_id+ " Product: " + res1[0].product_name,
-                        "Id#: "+res1[1].item_id+ " Product: " + res1[1].product_name,
-                        "Id#: "+res1[2].item_id+ " Product: " + res1[2].product_name,
-                        "Id#: "+res1[3].item_id+ " Product: " + res1[3].product_name,
-                        "Id#: "+res1[4].item_id+ " Product: " + res1[4].product_name,
-                        "Id#: "+res1[5].item_id+ " Product: " + res1[5].product_name,
-                        "Id#: "+res1[6].item_id+ " Product: " + res1[6].product_name,
-                        "Id#: "+res1[7].item_id+ " Product: " + res1[7].product_name,
-                        "Id#: "+res1[8].item_id+ " Product: " + res1[8].product_name,
-                        "Id#: "+res1[9].item_id+ " Product: " + res1[9].product_name,
-                    ]
-                }).then(function(res11) {
-                    
+                conn.query("SELECT * FROM products", function (req, res1) {
+
                     inquirer.prompt({
-                        name: "adding1",
-                        type: "input",
-                        message: "How much would you like to add to " + res11.adding + "?"
-                    }).then(function(res111) {
-                        
-                        const regex = /(?<=Id#: )(.*)(?= Product:)/g;
-                        const selected = parseInt(res11.adding.match(regex));
-                        const originalQuantity = parseInt(res1[0].stock_quantity);
-                        const addedQuantity = (parseInt(res111.adding1) + originalQuantity);
-                        console.log(addedQuantity);
+                        name: "adding",
+                        type: "list",
+                        message: "Which of the items would you like to add more to stock?",
+                        choices: [
+                            "Id#: " + res1[0].item_id + " Product: " + res1[0].product_name,
+                            "Id#: " + res1[1].item_id + " Product: " + res1[1].product_name,
+                            "Id#: " + res1[2].item_id + " Product: " + res1[2].product_name,
+                            "Id#: " + res1[3].item_id + " Product: " + res1[3].product_name,
+                            "Id#: " + res1[4].item_id + " Product: " + res1[4].product_name,
+                            "Id#: " + res1[5].item_id + " Product: " + res1[5].product_name,
+                            "Id#: " + res1[6].item_id + " Product: " + res1[6].product_name,
+                            "Id#: " + res1[7].item_id + " Product: " + res1[7].product_name,
+                            "Id#: " + res1[8].item_id + " Product: " + res1[8].product_name,
+                            "Id#: " + res1[9].item_id + " Product: " + res1[9].product_name,
+                        ]
+                    }).then(function (res11) {
 
-                        conn.query(`UPDATE products SET stock_quantity = ${addedQuantity} WHERE item_id = ${selected}`, function(req,res) {
+                        inquirer.prompt({
+                            name: "adding1",
+                            type: "input",
+                            message: "How much would you like to add to " + res11.adding + "?"
+                        }).then(function (res111) {
 
-                            console.log(`Updated Product to ${addedQuantity}`);
-                        });
-                        
-                        setTimeout(repeatToFront, 2000);
+                            const regex = /(?<=Id#: )(.*)(?= Product:)/g;
+                            const selected = parseInt(res11.adding.match(regex));
+                            const originalQuantity = parseInt(res1[0].stock_quantity);
+                            const addedQuantity = (parseInt(res111.adding1) + originalQuantity);
+                            console.log(addedQuantity);
+
+                            conn.query(`UPDATE products SET stock_quantity = ${addedQuantity} WHERE item_id = ${selected}`, function (req, res) {
+
+                                console.log(`Updated Product to ${addedQuantity}`);
+                            });
+
+                            setTimeout(repeatToFront, 2000);
+                        })
+
                     })
-
-                })
-            });
+                });
                 break;
 
             case "Add New Product":
 
-            inquirer.prompt([
-                {
-                    name: "input",
-                    message: "What product would you like to add to your inventory?",
-                    type: "input"
-                },
-                {
-                    type: "input",
-                    message: "What is the price for this item?",
-                    name: "price"
-                },
-                {
-                    type: "input",
-                    message: "How many of this item would you like to stock into your inventory?",
-                    name: "stock"
-                },
-                {
-                    type: "input",
-                    message: "What is the Department Name for this item?",
-                    name: "depart"
-                },
-                {
-                    type: "confirm",
-                    message: "Are you sure?",
-                    name: "confirm",
-                    default: true
-                }
-            ]).then(function(response) {
-                const a = response.input, b = response.depart, c = response.price, d = response.stock;
-                
+                inquirer.prompt([
+                    {
+                        name: "input",
+                        message: "What product would you like to add to your inventory?",
+                        type: "input"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the price for this item?",
+                        name: "price"
+                    },
+                    {
+                        type: "input",
+                        message: "How many of this item would you like to stock into your inventory?",
+                        name: "stock"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the Department Name for this item?",
+                        name: "depart"
+                    },
+                    {
+                        type: "confirm",
+                        message: "Are you sure?",
+                        name: "confirm",
+                        default: true
+                    }
+                ]).then(function (response) {
+                    const a = response.input, b = response.depart, c = response.price, d = response.stock;
 
-                conn.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('${a}', '${b}', '${c}', '${d}')`);
+                    conn.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('${a}', '${b}', '${c}', '${d}')`);
 
-                console.log(`\nNew Product has been added!\n=-=-=-=-=-=-=-=\nProduct: ${a}\nDepartment: ${b}\nPrice: ${c}\nStock Quantity: ${d}\n`);
+                    console.log(`\nNew Product has been added!\n=-=-=-=-=-=-=-=\nProduct: ${a}\nDepartment: ${b}\nPrice: ${c}\nStock Quantity: ${d}\n`);
 
-                setTimeout(repeatToFront, 4000);
-            });
+                    setTimeout(repeatToFront, 4000);
+                });
                 break;
 
-                default:
+            default:
                 process.exit();
         }
     })
